4bebf12dab9b31b2c20d1883ddf4900a72021ac7
Killian Habasque <killian.habasque@gmail.com>
2024-02-29 14:34:25 +0100

    feat(test): add test.yml

--- test/my-test.spec.ts
+++ test/my-test.spec.ts
@@ -3,18 +3,18 @@ import { setup, createPage } from '@nuxt/test-utils/e2e'
 
 describe('My test', async () => {
   await setup({
-    browser: true, // Activer le navigateur pour les tests de bout en bout
+    browser: true,
     browserOptions: {
-      type: 'chromium' // Vous pouvez également utiliser 'firefox' ou 'webkit'
+      type: 'chromium' 
     }
   })
 
   test('my test', async () => {
     
-    const page = await createPage('/') // Créer une instance de navigateur Playwright
-    // await page.goto('http://localhost:3000/') // Naviguer vers l'URL de votre application Nuxt
+    const page = await createPage('/') 
+  
     const test = await createPage('/test')
     await test.getByRole('link', { name: 'test' }).click();
-    // Écrire vos assertions ou interactions avec la page ici
+
   })
 })
